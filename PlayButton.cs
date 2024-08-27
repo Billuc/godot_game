491a7cc80d45b592969b5d0badcd21a4e13b799e
LUC BILLAUD <luc.billaud@univ-lyon1.fr>
2024-08-27 17:32:07 +0200

    Button themes and scene transitions

---
+++ PlayButton.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+
+public partial class PlayButton : Button
+{
+	private SceneTransitionRect _transitionRect;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_transitionRect = GetNode<SceneTransitionRect>("/root/Control/SceneTransitionRect");
+		Pressed += OnPressed;
+	}
+	
+	private async void OnPressed()
+	{
+		await _transitionRect.TransitionTo("res://start.tscn");
+	}
+}
---
+++ SceneTransitionRect.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+
+public partial class SceneTransitionRect : ColorRect
+{
+	// Path to the next scene to transition to
+	[Export(PropertyHint.File, "*.tscn")]
+	public string NextScenePath { get; set; }
+
+	// Reference to the AnimationPlayer node
+	private AnimationPlayer _animPlayer;
+
+	public override void _Ready()
+	{
+		_animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		_animPlayer.PlayBackwards("fade");
+	}
+
+	public async Task TransitionTo(string nextScene = null)
+	{
+		string sceneToLoad = nextScene ?? NextScenePath;
+		GD.Print("hello");
+		_animPlayer.Play("fade");
+		await ToSignal(_animPlayer, "animation_finished");
+		GetTree().ChangeSceneToFile(sceneToLoad);
+	}
+}
