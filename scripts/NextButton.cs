52b743063221536f5d3d10d21346945497785b07
LUC BILLAUD <luc.billaud@univ-lyon1.fr>
2024-09-03 18:22:26 +0200

    Added end scene

--- scripts/NextButton.cs
+++ scripts/NextButton.cs
@@ -1,3 +1,4 @@
+using FYBF;
 using Godot;
 using System;
 using System.Threading.Tasks;
@@ -17,7 +18,10 @@ public partial class NextButton : Button
 
 	private async void OnPressed()
 	{
-		await _transitionRect.TransitionTo("res://scenes/transportation_choice.tscn");
+		if (GameState.GetInstance().GetCurrentCharacter().Character == "Luc")
+			await _transitionRect.TransitionTo("res://scenes/end.tscn");
+		else
+			await _transitionRect.TransitionTo("res://scenes/transportation_choice.tscn");
 	}
 
 	public async Task ShowWithAnimation()
